6d5f74544245eaaf0672255f8c89977268c86cad
Abhishek Garg <abhishek@itadmins-MacBook-Pro.local>
2022-10-20 12:30:46 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+import AllInOneSDKManager from 'paytm_allinone_react-native';
+import {generateToken} from './Service';
+import {CALLBACK_URL, MID, URL_SCHEME} from './Constant';
+
+const Payment = () => {
+  const payNow = async () => {
+    let orderId = 'Test7';
+    let amt = 10;
+
+    const token = await generateToken(orderId, amt);
+
+    try {
+      AllInOneSDKManager.startTransaction(
+        orderId,
+        MID,
+        token,
+        amt.toFixed(1),
+        CALLBACK_URL + orderId,
+        true,
+        true,
+        URL_SCHEME,
+      )
+        .then(result => {
+          console.log('gateway response', result);
+        })
+        .catch(err => {
+          console.log('gateway error', err);
+        });
+    } catch (error) {
+      console.log('try catch error', error);
+    }
+  };
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button title="Create Payment" onPress={payNow} />
+    </View>
+  );
+};
+
+export default Payment;
---
+++ Paytm_Backend/keys.php
@@ -0,0 +1,4 @@
+<?php 
+$mid = "BYcDla35417521178605";
+$merchant_Key = "98&ne2uV%%bhvH8i";
+?>
